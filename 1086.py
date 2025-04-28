0c3688951fa2620bda4fb425670a20b2eae022f2
GitSkyBlue <hiskyb@naver.com>
2025-04-28 18:19:53 +0900

    1

--- 1086.py
+++ 1086.py
@@ -1,55 +1,56 @@
-# from itertools import permutations
-
-# n = int(input())
-
-# lists = []
-
-# for i in range(n):
-#     lists.append(input())
-
-# k = int(input())
-
-# sl = list(permutations(lists, len(lists)))
-# print(sl)
-# cor = 0
-# for l in sl:
-#     s = ''.join(l)
-#     if int(s) % k == 0:
-#         cor += 1
-
-# print(Fraction(cor, len(sl)))
-
-import sys, math
-
-sys.setrecursionlimit(int(1e6))
-input = sys.stdin.readline
+from itertools import permutations
+from fractions import Fraction
 
 n = int(input())
 
-arr = []
+lists = []
 
 for i in range(n):
-    arr.append(input().rstrip())
+    lists.append(input())
 
 k = int(input())
 
 cor = 0
 count = 0
+for l in permutations(lists, len(lists)):
+    s = ''.join(l)
+    if int(s) % k == 0:
+        cor += 1
+    count += 1
 
-def generate_permutations(arr, idx=0):
-    global cor, count
+print(Fraction(cor, count))
 
-    if idx == len(arr):
-        if int(''.join(arr)) % k == 0:
-            cor += 1
-        count += 1
-        return
+# import sys, math
 
-    for i in range(idx, len(arr)):
-        arr[idx], arr[i] = arr[i], arr[idx]
-        generate_permutations(arr, idx + 1)
-        arr[idx], arr[i] = arr[i], arr[idx]
+# sys.setrecursionlimit(int(1e6))
+# input = sys.stdin.readline
 
-generate_permutations(arr)
-mod = math.fmod(cor, count)
-print(f'{int(cor / mod)}/{int(count / mod)}')
+# n = int(input())
+
+# arr = []
+
+# for i in range(n):
+#     arr.append(input().rstrip())
+
+# k = int(input())
+
+# cor = 0
+# count = 0
+
+# def generate_permutations(arr, idx=0):
+#     global cor, count
+
+#     if idx == len(arr):
+#         if int(''.join(arr)) % k == 0:
+#             cor += 1
+#         count += 1
+#         return
+
+#     for i in range(idx, len(arr)):
+#         arr[idx], arr[i] = arr[i], arr[idx]
+#         generate_permutations(arr, idx + 1)
+#         arr[idx], arr[i] = arr[i], arr[idx]
+
+# generate_permutations(arr)
+# mod = math.fmod(cor, count)
+# print(f'{int(cor / mod)}/{int(count / mod)}')
--- 2579.py
+++ 2579.py
@@ -2,14 +2,15 @@ from collections import deque
 
 n = int(input())
 score_list = deque([int(input()) for _ in range(n)])
-dp = [0] * (n + 1)
+dp = [0] * (n)
 
-dp[0] = score_list[0]
-dp[1] = score_list[0] + score_list[1]
-dp[2] = max(score_list[1] + score_list[3], score_list[2] + score_list[3])
-for i in range(3, n):
-    print(i)
-    dp[i] = max(dp[i - 2] + score_list[i], dp[i - 3] + score_list[i - 1] + score_list[i])
+if n < 3:
+    print(sum(score_list))
+else:
+    dp[0] = score_list[0]
+    dp[1] = score_list[0] + score_list[1]
+    dp[2] = max(score_list[0] + score_list[2], score_list[1] + score_list[2])
+    for i in range(3, n):
+        dp[i] = max(dp[i - 2] + score_list[i], dp[i - 3] + score_list[i - 1] + score_list[i])
 
-print(score_list)
-print(dp)
+    print(dp[n - 1])
