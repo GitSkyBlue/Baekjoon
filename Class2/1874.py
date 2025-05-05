9ce66774e0c55ecd5a4509b7dc5a0def2aa7b401
BuildTools <unconfigured@null.spigotmc.org>
2025-05-05 21:51:07 +0900

    Hello World!

--- Class2/1874.py
+++ Class2/1874.py
@@ -4,12 +4,32 @@ from collections import deque
 number_list = list(map(int, sys.stdin.read().splitlines()))[1:]
 
 dq = deque()
+answer_list = []
+current = 0
+target = 0
+while True:
+    if len(dq) == 0:
+        current += 1
+        dq.append(current)
+        answer_list.append('+')
 
-print(number_list)
-current = number_list[0]
-for i in range(current):
-    dq.append(i + 1)
-print(dq, current)
-for i in number_list:  
-    pass
+    if current >= number_list[target]:
+        answer_list.append('-')
+        num = dq.pop()
+        if num == number_list[target]:
+            target += 1
+        else:
+            print('NO')
+            break
+    elif current < number_list[target]:
+        answer_list.append('+')
+        current += 1
+        dq.append(current)
+        if current > len(number_list) + 1:
+            print('NO')
+            break
 
+    if len(answer_list) == len(number_list) * 2:
+        for a in answer_list:
+            print(a)
+        break
--- Class2/7568.py
+++ Class2/7568.py
@@ -1,28 +1,17 @@
-import sys
-
 n = int(input())
-numbers = sys.stdin.read().splitlines()
 
-new_numbers = [list(map(int, i.split())) for i in numbers]
-big_dict = {}
-max_w = 0
-max_h = 0
-current_i = 1
-for i, (w, h) in enumerate(sorted(new_numbers, reverse=True)):
-    if max_w < w and max_h < h:
-        max_w = w
-        max_h = h
-        big_dict[str(w) + ' ' + str(h)] = current_i
-    elif max_w > w and max_h <= h:
-        big_dict[str(w) + ' ' + str(h)] = current_i
-    elif max_w > w and max_h > h:
-        current_i = i + 1
-        max_w = w
-        max_h = h
-        big_dict[str(w) + ' ' + str(h)] = current_i
-print(sorted(new_numbers, reverse=True))
-s = ''
+number_list = []
+answer_list = []
 
-for n in numbers:
-    s += str(big_dict[n]) + ' '
-print(s.strip())
+for i in range(n):
+    h, w = map(int, input().split())
+    number_list.append([h, w])
+
+for number in number_list:
+    rank = 1
+    for compare in number_list:
+        if number[0] < compare[0] and number[1] < compare[1]:
+            rank += 1
+    answer_list.append(rank)
+
+print(str(answer_list).strip('[]').replace(',', ''))
