3255ebbe12fbbadf602e5f4a4738d686ef8ace53
GitSkyBlue <hiskyb@naver.com>
2025-05-10 16:35:07 +0900

    Hello World!

--- Class2/1654.py
+++ Class2/1654.py
@@ -4,20 +4,21 @@ n, m = map(int, sys.stdin.readline().strip().split())
 
 length_list = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
-low = 0
+low = 1
 high = max(length_list)
-count = 0
-while abs(high - low) > 2 and count != m:
-    mid = low + (high - low) // 2
+result = 0
+while low <= high:
     count = 0
+
+    mid = low + (high - low) // 2
     for length in length_list:
         count += length // mid
-
-    if m > count:
-        high = mid
-    elif m <= count:
-        low = mid
-    print(count, mid, low, high)
-print(count, mid, low, high)
     
-sys.stdout.write(str(mid))
+    if count >= m:
+        result = mid
+        low = mid + 1
+    else:
+        high = mid - 1
+        
+sys.stdout.write(str(result))
+
